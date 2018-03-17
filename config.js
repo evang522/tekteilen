40da914e06c564faf69f26fda7c95ad4ef055276
Evan Garrett <evang522@gmail.com>
2018-03-16 20:05:08 -0400

    Made changes to configs to allow check for env variables on deploy, if not default to elephantsql

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   DBOBJ: {
-    url: `postgresql://${process.env.DBUSER}:${process.env.DBUSERPASS}@${process.env.DBHOST}:${process.env.DBPORT || 5432}/${process.env.DBNAME}` || 'postgres://jnqlwygy:pYdrcHZoA5ACTQ-lHwjDN5BrNpZuYNMc@stampy.db.elephantsql.com:5432/jnqlwygy'
+    url: process.env.DBUSER ? `postgresql://${process.env.DBUSER}:${process.env.DBUSERPASS}@${process.env.DBHOST}:${process.env.DBPORT || 5432}/${process.env.DBNAME}`:  'postgres://jnqlwygy:pYdrcHZoA5ACTQ-lHwjDN5BrNpZuYNMc@stampy.db.elephantsql.com:5432/jnqlwygy'
   },
   JWT_SECRET: process.env.SECRET,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN
