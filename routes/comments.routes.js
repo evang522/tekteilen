1e50f3bdb76fbf07e92ad95b9e5aaf59da78c886
Evan Garrett <evang522@gmail.com>
2018-03-19 17:54:50 -0400

    Add logic for adding comments

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -8,10 +8,48 @@ const knex = require('../db/connect');
 
 //==================================GET ALL COMMENTS=========================>
 router.get('/comments', (req,res,next) => {
-  knex('comments')
-    .select('id','author_id','project_id','message')
+  knex
+    .select('comments.id','user_id','project_id','message', 'users.fullname as author')
+    .from('comments')
+    .leftJoin('users','users.id','comments.user_id')
     .then(response => {
       res.status(200).json(response);
+    });
+});
+
+
+
+router.post('/comments', (req,res,next) => {
+  let newCommentId;
+  const {userId, projectId, commentBody} = req.body;
+  return knex('comments')
+    .insert({
+      'user_id':userId,
+      'project_id':projectId,
+      'message': commentBody
+    })
+    .returning(['user_id','project_id','message','date','id'])
+    .then(response => {
+      newCommentId = response[0].id;
+      res.status(201).json(response[0]);
+    })
+    .then(() => {
+      return knex('projects')
+        .where('id',projectId)
+        .update({
+          comments: knex.raw('array_append("comments", ? )', [`${newCommentId}`])
+        })
+        .then(response => {
+          if (response === 0) {
+            const err = new Error();
+            err.status = 404;
+            err.message = 'A project with this ID could not be found.';
+            return Promise.reject(err);
+          }
+        })
+        .catch(err => {
+          next(err);
+        });
     });
 });
 
