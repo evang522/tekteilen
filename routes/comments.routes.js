2784cdc2d0c354fa7b91687f2768324233ae3862
Evan Garrett <evang522@gmail.com>
2018-03-21 23:43:19 -0400

    Altered comment route to get rid of unnecessary code

--- routes/comments.routes.js
+++ routes/comments.routes.js
@@ -19,9 +19,8 @@ router.get('/comments', (req,res,next) => {
 });
 
 
-
+// Create comment in comment table
 router.post('/comments', (req,res,next) => {
-  let newCommentId;
   const {userId, projectId, commentBody} = req.body;
   return knex('comments')
     .insert({
@@ -31,26 +30,7 @@ router.post('/comments', (req,res,next) => {
     })
     .returning(['user_id','project_id','message','date','id'])
     .then(response => {
-      newCommentId = response[0].id;
       res.status(201).json(response[0]);
-    })
-    .then(() => {
-      return knex('projects')
-        .where('id',projectId)
-        .update({
-          comments: knex.raw('array_append("comments", ? )', [`${newCommentId}`])
-        })
-        .then(response => {
-          if (response === 0) {
-            const err = new Error();
-            err.status = 404;
-            err.message = 'A project with this ID could not be found.';
-            return Promise.reject(err);
-          }
-        })
-        .catch(err => {
-          next(err);
-        });
     });
 });
 
@@ -62,7 +42,6 @@ router.delete('/comments/:id', (req,res,next) => {
     .where('id',commentId)
     .select('user_id')
     .then(response => {
-      console.log('response: ', response[0], commentId);
       if (response[0].user_id !== req.user.id) {
         const err = new Error();
         err.message = 'Comments made by other users may not be deleted';
