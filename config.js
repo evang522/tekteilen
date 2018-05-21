5dd19308fbe69997f3b891935a7065a9cb54279e
Evan Garrett <evang522@gmail.com>
2018-05-21 19:11:41 -0400

    Fix DB URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   DBOBJ: {
-    url: process.env.DBUSER ? `postgresql://${process.env.DBUSER}:${process.env.DBUSERPASS}@${process.env.DBHOST}:${process.env.DBPORT || 5432}/${process.env.DBNAME}`:  'postgres://jnqlwygy:pYdrcHZoA5ACTQ-lHwjDN5BrNpZuYNMc@stampy.db.elephantsql.com:5432/jnqlwygy',
+    url: process.env.DBURL,
     testurl: `postgresql://${process.env.DBUSER}:${process.env.DBUSERPASS}@${process.env.DBHOST}:${process.env.DBPORT || 5432}/tekteilentest`
   },
   JWT_SECRET: process.env.SECRET,
